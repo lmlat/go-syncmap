07de4547ab16386a2264b968749c44b215271c13
1003448731@qq.com <1003448731@qq.com>
2023-07-17 11:44:27 +0800

    update doc and add some test case

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# syncmap
+# 介绍
 sync.Map 是 Go 语言标准库中的一个并发安全的键值对集合, 用于在并发环境下进行读取和写入操作。
-自官方在 1.9 加入了 sync.Map 之后, 就一直没有获取键值对数量的方法, 导致每次都需要调用 Range 方法来统计, 为了解决这个简单而又繁琐的问题, 基于 Go1.20 对 sync.Map 新增了 Len 方法, 用于获取 sync.Map 中存储的键值对个数。
+自官方在 1.9 加入了 sync.Map 之后, 就一直没有获取键值对数量的方法, 导致每次都需要调用 Range 方法来统计, 为了解决这个简单而又繁琐的问题, 基于 1.20 对 sync.Map 新增了 Len 方法, 主要用于获取 sync.Map 中存储的键值对个数。
 
 # 原理
 在 sync.Map 类型中增加了一个 int64 类型的属性 size, 分别在 Swap、LoadOrStore、LoadAndDelete、CompareAndDelete、dirtyLocked、missLocked方法中新增了对 size 属性的处理逻辑。
+
+# 用法
+```go
+// 实例化
+m := new(ts.Map)
+// 添加键值对
+m.Store("name", "aitao")
+m.Store("age", 100)
+// 打印键值对内容
+fmt.Println(m.String()) // {}
+// 打印键值对数量
+fmt.Println(m.Len()) // 2
+```
--- syncmap_test.go
+++ syncmap_test.go
@@ -1,7 +1,8 @@
-package ts
+package ts_test
 
 import (
 	"fmt"
+	ts "github.com/lmlat/syncmap"
 	"math/rand"
 	"sync"
 	"sync/atomic"
@@ -14,13 +15,25 @@ import (
  * @Date 2023/7/17 7:47
  * @Url
  **/
-func TestMap_Len3(t *testing.T) {
+func TestMap_Len2(t *testing.T) {
+	// 实例化
+	m := new(ts.Map)
+	// 添加键值对
+	m.Store("name", "aitao")
+	m.Store("age", 100)
+	// 打印键值对内容
+	fmt.Println(m.String()) // {name=aitao, age=100}
+	// 打印键值对数量
+	fmt.Println(m.Len()) // 2
+}
+
+func TestMap_Len(t *testing.T) {
 	list := map[string]interface{}{
 		"a": "aaa",
 		"b": "bbb",
 		"c": "ccc",
 	}
-	var m Map
+	var m ts.Map
 	for k, v := range list {
 		m.LoadOrStore(k, v)
 	}
@@ -40,56 +53,31 @@ func TestMap_Len3(t *testing.T) {
 	}()
 	wg.Wait()
 }
-func TestMap_Len2(t *testing.T) {
-	m := new(Map)
-	for i := 0; i < 10; i++ {
-		m.LoadOrStore(i, i)
-	}
-	m.Range(func(key, value any) bool {
-		fmt.Println(key, value)
-		return true
-	})
-	fmt.Println(m.Len())
-}
-func TestMap_Len(t *testing.T) {
-	// 创建一个并发安全的 Map
-	m := new(Map)
 
-	// 设置并发读写操作的数量
-	const numOps = 1000
-
-	// 使用原子计数器来统计成功读写操作的数量
-	var successOps int64
-
-	// 使用 WaitGroup 来等待所有 goroutine 完成
+func TestMap_Concurrency(t *testing.T) {
+	m := new(ts.Map)
+	// 统计成功读写操作的数量
+	var count int64
 	var wg sync.WaitGroup
-	wg.Add(numOps)
-
+	wg.Add(1000)
 	// 启动多个 goroutine 进行并发读写操作
-	for i := 0; i < numOps; i++ {
+	for i := 0; i < 1000; i++ {
 		go func() {
-			// 生成一个随机数作为 key
+			defer wg.Done()
 			key := rand.Intn(10)
-
 			// 进行并发的读写操作
 			if rand.Float64() < 0.5 {
 				m.Store(key, "value")
-				atomic.AddInt64(&successOps, 1)
+				atomic.AddInt64(&count, 1)
 			} else {
 				_, ok := m.Load(key)
 				if ok {
-					atomic.AddInt64(&successOps, 1)
+					atomic.AddInt64(&count, 1)
 				}
 			}
-
-			// 标记 goroutine 完成
-			wg.Done()
 		}()
 	}
-
-	// 等待所有 goroutine 完成
 	wg.Wait()
 	fmt.Println(m.Len())
-	// 输出成功读写操作的数量
-	fmt.Println("Successful operations:", successOps)
+	fmt.Println("成功数:", count)
 }
